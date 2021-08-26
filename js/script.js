7ecece6c68e58de8df977eac1d55d3242c8e2528
pizzanumerouno <klara.kolevska@centrum.cz>
2021-08-26 10:15:55 +0200

    drt

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ function mariovisibility(){
     mario.classList.toggle("hidden")
     mariotext.classList.toggle("hidden")
 }
+
+let luigi =document.getElementById("luigi")
+let luigitext =document.getElementById("luigitext")
+ 
+
+function luigivisibility(){
+    luigi.classList.toggle("hidden")
+    luigitext.classList.toggle("hidden")
+}
